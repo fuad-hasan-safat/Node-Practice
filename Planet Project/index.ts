b922c2f0f5462cd583f50158d36c6535497e43f2
Fuad Hasan <47121920+fuad-hasan-safat@users.noreply.github.com>
2024-09-12 17:00:32 +0600

    Deno server

--- Planet Project/index.ts
+++ Planet Project/index.ts
@@ -1,45 +1,42 @@
-import { parse } from "jsr:@std/csv"
+import { parse } from "jsr:@std/csv";
+
 async function readCSV(filePath: string) {
-
   const data = await Deno.readTextFile(filePath);
-  const [headerRow, ...dataRows] = data.trim().split('\n');
-  console
+  const allRow = data.trim().split("\n");
+  let headerRow = "";
+  for (var line of allRow) {
+    if (line[0] !== "#") {
+      headerRow = line;
+      break;
+    }
+  }
 
-const columns = headerRow.split(',');
+  const columns = headerRow.split(",");
 
   const records = parse(data, {
-    //skipFirstRow: true,
-    comment: '#',
-    columns: columns.map(col => col.trim()),
+    comment: "#",
+    columns: columns.map((col) => col.trim()),
   });
 
-  console.log('record : ', records)
-  
   return records;
 }
 
 const filePath = "kepler_data.csv";
 const csvData = await readCSV(filePath);
-
-
-
 const habitablePlanets = [];
 
 function isHabitablePlanet(planet: any) {
-  return planet['koi_disposition'] === 'CONFIRMED'
-    && planet['koi_insol'] > 0.36 && planet['koi_insol'] < 1.11
-    && planet['koi_prad'] < 1.6;
-}
-let i = 0
-for(const planet in csvData){
-  if(i<20){
-    console.log(planet)
-
-  }
-i = i + 1
-  if(isHabitablePlanet(planet)){
-    habitablePlanets.push(planet)
-  }
+  return planet["koi_disposition"] === "CONFIRMED" &&
+    planet["koi_insol"] > 0.36 && planet["koi_insol"] < 1.11 &&
+    planet["koi_prad"] < 1.6;
 }
 
+for (const planet of csvData) {
+  if (isHabitablePlanet(planet)) {
+    habitablePlanets.push(planet);
+  }
+}
+console.log(habitablePlanets.map((planate) =>{
+  return planate['kepler_name']
+}))
 console.log(`${habitablePlanets.length} habitable planets found!`);
