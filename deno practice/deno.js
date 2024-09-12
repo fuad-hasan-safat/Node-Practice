e544fb148adf525c62794a4cc34e7c252fb9a117
Fuad Hasan <47121920+fuad-hasan-safat@users.noreply.github.com>
2024-09-12 09:16:42 +0600

    ryrhrety

--- deno practice/deno.js
+++ deno practice/deno.js
@@ -6,12 +6,16 @@
 // }
 
 const food = Deno.args[0];
-const parent = Deno.args[1]
-if (food === 'love' && parent === 'ryan') {
-   console.log('🦕...Deno is born!')
-}else{
-    console.log('..... this egg need some love')
+const parent = Deno.args[1];
+if (food === "love" && parent === "ryan") {
+    console.log("🦕...Deno is born!");
+} else {
+    console.log("..... this egg need some love");
 }
 
+setTimeout(() => {
+    console.log("check");
+    console.table(Deno.metrics());
+}, 1000);
 
-console.table(Deno.metrics());
+
