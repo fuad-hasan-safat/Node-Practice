68787de7c6dab10321dc3a37f2984378030bfe95
Fuad Hasan <47121920+fuad-hasan-safat@users.noreply.github.com>
2024-09-22 18:00:02 +0600

    Update index.ts

--- Nasa Project/nasa_project/src/index.ts
+++ Nasa Project/nasa_project/src/index.ts
@@ -2,14 +2,12 @@ import { Elysia , t} from 'elysia'
 import { swagger } from '@elysiajs/swagger'
 import { note } from './routes/note'
 
+const PORT = process.env.PORT || 8000
+
 const app = new Elysia()
-    // Apply the swagger plugin
-    .use(swagger()) 
-    .get('/', ({ path }) => `${path} is called here`)
-    .use(note)
-    .listen(3000)
+    
 
-
+app.listen(PORT)
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
 );
